5c2bc11aea4ce6caaf72b043f9c130a22cea5b60
Pamuda Balasuriya <pamuda1995@gmail.com>
2025-09-25 23:22:28 +0530

    commit3

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, World! This is a sample Flask app2."
+    return "Hello from AWS Elastic Beanstalk!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
