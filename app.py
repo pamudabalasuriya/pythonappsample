e2245b8c96c39ccbfe6a91bab409e84f78cf4863
Pamuda Balasuriya <pamuda1995@gmail.com>
2025-09-25 23:24:52 +0530

    commit4

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from AWS Elastic Beanstalk!"
+    return "Hello from AWS Elastic Beanstalk app2!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
